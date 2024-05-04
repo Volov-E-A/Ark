876540e8d5176e54d3f9a31fe1257373a5043a19
Egor Alekseevich <86200806+Volov-E-A@users.noreply.github.com>
2024-05-04 23:07:45 +0300

    some add js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const form = document.querySelector(".main__qustionForm_form") //поиск по селектору
+const cards = document.querySelectorAll(".main__services .main__services_cart")
+const modal = document.querySelector(".overlay")
 // console.log(form);
 function formSubmit(event){
     event.preventDefault() // сброс настроек отправки формы
@@ -7,4 +9,32 @@ function formSubmit(event){
     
 }
 
+
+
+function eventCard (event){
+    const card = event.currentTarget
+    modal.classList.remove("hidden")    
+    const titleText = card.querySelector(".main__services_cart-title").textContent
+    const buttonText = card.querySelector(".main__services_cart-price").textContent
+    const pText = card.querySelector(".main__services_cart-text").textContent
+    const modalTitle = modal.querySelector(".main__services_cart-title")
+    const modalBtn = modal.querySelector(".main__services_cart-price")
+    const modalText = modal.querySelector(".main__services_cart-text")
+    modalTitle.textContent = titleText
+    modalBtn.textContent = buttonText
+    modalText.textContent = pText
+    modalBtn.classList.add("main__services_cart-price-open")
+}
+
+cards.forEach((card) => card.addEventListener("click", eventCard ))
 form.addEventListener("submit", formSubmit ) //слушатель события при отправке формы
+modal.addEventListener("click", (e) => {
+    if(e.target.classList.contains('overlay')){
+        modal.classList.add("hidden")
+    }
+})
+
+
+// оформить текст в карточка в соотвтествии с макетом
+// текст не должен быты отображен в карточках
+// настроить передачу текста карточки в модально окно при клике
